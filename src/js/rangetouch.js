3ab517bdd115ee6287d55187268c144442ec3d63
Sam Potts <sam@potts.es>
2017-06-12 17:03:49 +0700

    Published v1.0.3

--- src/js/rangetouch.js
+++ src/js/rangetouch.js
@@ -1,5 +1,5 @@
 // ==========================================================================
-// rangetouch.js v1.0.2
+// rangetouch.js v1.0.3
 // Making <input type="range"> work on touch devices
 // https://github.com/selz/rangetouch
 // License: The MIT License (MIT)
